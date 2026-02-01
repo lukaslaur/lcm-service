c7c7564fb59f62475998df343999faabb8983f83
lukaslaur <56416612+lukaslaur@users.noreply.github.com>
2026-02-01 09:53:50 +0200

    Remove helper functions comment from server.js
    Removed commented-out section for helper functions.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Helper functions
 function isNaturalNumber(str) {
     const num = Number(str);
     return Number.isInteger(num) && num >= 1;
